57ea31d2b2c40a861e4434db21ad66bb863937aa
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2020-03-19 14:50:50 -0300

    Start commit. Working on Header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import './style.css';
+
+class Header extends Component {
+  render() {
+    return <header>Teste</header>;
+  }
+}
+
+export default Header;
